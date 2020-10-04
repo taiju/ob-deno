a7d9446280759cf8cde1fd1237e17711dc6afc3e
HIGASHI Taiju <higashi@taiju.info>
2020-10-05 06:52:12 +0900

    fix: suppress lint errors and warnings

--- ob-deno.el
+++ ob-deno.el
@@ -3,8 +3,10 @@
 ;; Copyright (C) 2020 Free Software Foundation, Inc.
 
 ;; Author: HIGASHI Taiju
-;; Keywords: literate programming, reproducible research, js, ts
+;; Keywords: literate programming, reproducible research, javascript, typescript, tools
 ;; Homepage: https://github.com/taiju/ob-deno
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "26.1"))
 
 ;; This file is part of GNU Emacs.
 
@@ -35,12 +37,12 @@
 (defvar org-babel-default-header-args:deno '()
   "Default header arguments for js/ts code blocks.")
 
-(defcustom org-babel-deno-cmd "deno"
+(defcustom ob-deno-cmd "deno"
   "Name of command used to evaluate js/ts blocks."
   :group 'ob-deno
   :type 'string)
 
-(defvar org-babel-deno-function-wrapper
+(defvar ob-deno-function-wrapper
   "Deno.stdout.write(new TextEncoder().encode(Deno.inspect((() => {%s})())));"
   "Javascript/TypeScript code to print value of body.")
 
@@ -48,7 +50,7 @@
   "Execute a block of Javascript/TypeScript code with org-babel.
 This function is called by `org-babel-execute-src-block'."
   (let* ((no-color-env (getenv "NO_COLOR"))
-	 (org-babel-deno-cmd (or (cdr (assq :cmd params)) (format "%s run" org-babel-deno-cmd)))
+	 (ob-deno-cmd (or (cdr (assq :cmd params)) (format "%s run" ob-deno-cmd)))
          (result-type (cdr (assq :result-type params)))
          (full-body (org-babel-expand-body:generic
 		     body params (org-babel-variable-assignments:deno params)))
@@ -57,17 +59,17 @@ This function is called by `org-babel-execute-src-block'."
 		     (insert
 		      ;; return the value or the output
 		      (if (string= result-type "value")
-			  (format org-babel-deno-function-wrapper full-body)
+			  (format ob-deno-function-wrapper full-body)
 			full-body)))
 		   (setenv "NO_COLOR" "true")
 		   (org-babel-eval
-		    (format "%s %s" org-babel-deno-cmd
+		    (format "%s %s" ob-deno-cmd
 			    (org-babel-process-file-name script-file)) ""))))
     (setenv "NO_COLOR" no-color-env)
     (org-babel-result-cond (cdr (assq :result-params params))
-      result (org-babel-deno-read result))))
+      result (ob-deno-read result))))
 
-(defun org-babel-deno-read (results)
+(defun ob-deno-read (results)
   "Convert RESULTS into an appropriate elisp value.
 If RESULTS look like a table, then convert them into an
 Emacs-lisp table, otherwise return the results as a string."
@@ -85,19 +87,19 @@ Emacs-lisp table, otherwise return the results as a string."
 					"'" "\"" results))))))
      results)))
 
-(defun org-babel-deno-var-to-deno (var)
+(defun ob-deno-var-to-deno (var)
   "Convert VAR into a js/ts variable.
 Convert an elisp value into a string of js/ts source code
 specifying a variable of the same value."
   (if (listp var)
-      (concat "[" (mapconcat #'org-babel-deno-var-to-deno var ", ") "]")
+      (concat "[" (mapconcat #'ob-deno-var-to-deno var ", ") "]")
     (replace-regexp-in-string "\n" "\\\\n" (format "%S" var))))
 
 (defun org-babel-variable-assignments:deno (params)
   "Return list of Javascript/TypeScript statements assigning the block's variables."
   (mapcar
    (lambda (pair) (format "var %s=%s;"
-			  (car pair) (org-babel-deno-var-to-deno (cdr pair))))
+			  (car pair) (ob-deno-var-to-deno (cdr pair))))
    (org-babel--get-vars params)))
 
 (provide 'ob-deno)
