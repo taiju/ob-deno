3107e2a71b69d32c548cc6bbaef4d1907afa51b3
HIGASHI Taiju <higashi@taiju.info>
2020-10-12 17:45:42 +0900

    add: package description

--- ob-deno.el
+++ ob-deno.el
@@ -12,7 +12,16 @@
 
 ;;; Commentary:
 
-;; ob-deno is based on ob-js.
+;; ob-deno is babel functions for Javascript/TypeScript with Deno.  It's based on ob-js.
+
+;; Parameters supported:
+;;   - :cmd
+;;   - :result-type
+;;   - :var (You can specify a variable prefix with ob-deno-variable-prefix.)
+;; Parameters not supported:
+;;   - :session
+;; Original Parameters:
+;;   - :allow (Specifies a permission list for the deno command)
 
 ;;; Requirements:
 
